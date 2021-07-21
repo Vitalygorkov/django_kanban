9a6e1a20dffc59f373154c9716bc55d0c573eacb
Vitalygorkov <vet55omsk@gmail.com>
2021-07-21 23:47:59 +0600

    Created a foreign key on the categories and added to the problem model

--- mysite/kanban/models.py
+++ mysite/kanban/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=50)
+    category_slug = models.CharField(max_length=50, default=1)
+
+    def __str__(self):
+        return self.category_name
+
+class Task(models.Model):
+    task_name = models.CharField(max_length=800)
+    task_text = models.CharField(max_length=200)
+    task_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField('date published')
+    rating = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.task_name
