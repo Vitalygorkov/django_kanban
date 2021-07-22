b559c28aa3288491296108d89006210a2b5d6e6b
Vitalygorkov <vet55omsk@gmail.com>
2021-07-22 15:57:31 +0600

    Class Status has been created

--- mysite/kanban/models.py
+++ mysite/kanban/models.py
@@ -7,12 +7,20 @@ class Category(models.Model):
     def __str__(self):
         return self.category_name
 
+class Status(models.Model):
+    status_name = models.CharField(max_length=50)
+    status_slug = models.CharField(max_length=50, default=1)
+
+    def __str__(self):
+        return self.status_name
+
 class Task(models.Model):
     task_name = models.CharField(max_length=800)
     task_text = models.CharField(max_length=200)
-    task_category = models.ForeignKey(Category, on_delete=models.CASCADE)
     pub_date = models.DateTimeField('date published')
     rating = models.IntegerField(default=1)
+    task_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    task_status = models.ForeignKey(Status, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.task_name
