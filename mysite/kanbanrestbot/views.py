f573f293aad96e4e688066578e31395ae4c96ce9
Vitalygorkov <vet55omsk@gmail.com>
2021-07-31 23:23:28 +0600

    added line path('random'

--- mysite/kanbanrestbot/views.py
+++ mysite/kanbanrestbot/views.py
@@ -2,7 +2,7 @@
 
 from django.shortcuts import render
 from rest_framework import serializers
-from rest_framework.views import APIVIew
+from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from . import models
