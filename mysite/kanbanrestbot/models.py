2d3434447e55d0bcac5043a77bf437a7e5fe2de5
Vitalygorkov <vet55omsk@gmail.com>
2021-07-31 23:07:06 +0600

    The TaskSerializator and RandomTask classes are created in the kanbanrestbot /views.py file

--- mysite/kanbanrestbot/models.py
+++ mysite/kanbanrestbot/models.py
@@ -1,3 +1,28 @@
+# https://www.youtube.com/watch?v=fGXqTnWAFOo
+
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=50)
+    category_slug = models.CharField(max_length=50, default=1)
+
+    def __str__(self):
+        return self.category_name
+
+class Status(models.Model):
+    status_name = models.CharField(max_length=50)
+    status_slug = models.CharField(max_length=50, default=1)
+
+    def __str__(self):
+        return self.status_name
+
+class Task(models.Model):
+    task_name = models.CharField(max_length=800)
+    task_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    rating = models.IntegerField(default=1)
+    task_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    task_status = models.ForeignKey(Status, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.task_name
--- mysite/kanbanrestbot/views.py
+++ mysite/kanbanrestbot/views.py
@@ -1,3 +1,23 @@
+# https://www.youtube.com/watch?v=fGXqTnWAFOo
+
 from django.shortcuts import render
+from rest_framework import serializers
+from rest_framework.views import APIVIew
+from rest_framework.response import Response
+
+from . import models
+import random
 
 # Create your views here.
+
+class TaskSerializator(serializers.ModelSerializer):
+    class Meta:
+        model = models.Task
+        fields = ['task_name', 'task_text']
+
+class RandomTask(APIView):
+    def get(self, **args, **kwargs):
+        all_tasks = models.Task.objects.all()
+        random_task = random.choice(all_tasks)
+        serialized_random_task = TaskSerializator(random_task, many=False)
+        return Response(serialized_random_task.data)
