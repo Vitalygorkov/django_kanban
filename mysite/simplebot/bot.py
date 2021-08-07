c21edbf0192578c44a4d7e7b846bf986ec123079
Vitalygorkov <vet55omsk@gmail.com>
2021-08-07 20:14:38 +0600

    Adding a database to the bot.py file

--- mysite/simplebot/bot.py
+++ mysite/simplebot/bot.py
@@ -1,13 +1,32 @@
 # https://surik00.gitbooks.io/aiogram-lessons/content/chapter1.html
+# https://ru.wikibooks.org/wiki/SQLAlchemy
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
-
 from config import TOKEN
+
+import sqlalchemy
+# from sqlalchemy import create_engine
+engine = create_engine('sqlite:///:memory:', echo=True)
+print("Версия SQLAlchemy:", sqlalchemy.__version__)
+
+# from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
+metadata = MetaData()
+users_table = Table('tasks', metadata,
+    Column('id', Integer, primary_key=True),
+    Column('task_name', String),
+    Column('task_text', String),
+    Column('tsk_rating', String),
+    Column('tsk_category', String),
+    Column('tsk_status', String)
+)
+metadata.create_all(engine)
 
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
+
+
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
