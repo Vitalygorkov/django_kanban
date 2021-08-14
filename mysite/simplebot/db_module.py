fa6a2e13be424c75d0d96be06a7d66f20257cf0f
Vitalygorkov <vet55omsk@gmail.com>
2021-08-14 23:47:04 +0600

    trying to write to the database

--- mysite/simplebot/db_module.py
+++ mysite/simplebot/db_module.py
@@ -1,8 +1,12 @@
 # https://docs.sqlalchemy.org/en/14/core/engines.html
+# https://ru.wikibooks.org/wiki/SQLAlchemy
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import mapper
+from sqlalchemy.orm import mapper, sessionmaker
+
 engine = create_engine('sqlite:///mydatabase.db')
+Session = sessionmaker(bind=engine)
+Session.configure(bind=engine)
 
 from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
 metadata = MetaData()
@@ -26,6 +30,11 @@ class Task(object):
 print(mapper(Task, tasks_table))
 
 task1 = Task('make a bot for processing tasks', 'master the use of orm databases, write command handlers and bot logic')
+session = Session()
+session.add(task1)
+ourTask = session.query(Task).first()
+print(ourTask)
+
 
 print(task1)
 print(task1.id)
